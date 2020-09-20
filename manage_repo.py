bfd02006c399439cbe8a118929bbb7bcd26cae1e
Glenn Guy <glennguy83@gmail.com>
2020-09-21 09:37:33 +1000

    Update manage_repo.py

--- manage_repo.py
+++ manage_repo.py
@@ -311,7 +311,8 @@ def fetch_addon_from_folder(raw_addon_location, target_folder, build_location):
             build_location, addon_target_folder, addon_metadata)
     
     # set path for addons.xml
-    addons_path = addon_metadata.platform.find('..').SubElement('path')
+    addons_path = addon_metadata.root.find(
+        './addon/extension[@point="xbmc.addon.metadata"]')
     addons_path.text = os.path.join(
         addon_target_folder_basename, get_archive_basename(addon_metadata))
     return addon_metadata
