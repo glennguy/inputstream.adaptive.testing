db2710a6110d7f1f8db60bf594d860ea303c2be5
Glenn Guy <glennguy83@gmail.com>
2020-09-23 21:58:51 +1000

    separate deploy script

---
+++ build_scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+KODI_VERSION=$1
+mkdir -p $HOME/.deploy/$KODI_VERSION
+cd $HOME/.deploy/$KODI_VERSION
+for f in $TRAVIS_BUILD_DIR/*.zip; do
+  if [[ $f == *"$KODI_VERSION"* ]]; then
+    unzip $f -d $TRAVIS_BUILD_DIR/.build/$KODI_VERSION/$(basename "$f" .zip)
+    python3 $TRAVIS_BUILD_DIR/manage_repo.py $TRAVIS_BUILD_DIR -b $TRAVIS_BUILD_DIR/.build/$KODI_VERSION/$(basename "$f" .zip)/${app_id}
+  fi
+done
