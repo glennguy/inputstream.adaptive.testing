e4228018275f96ca57df2d3a6a1c01160d9905e3
Glenn Guy <glennguy83@gmail.com>
2020-09-24 14:46:13 +1000

    create folder

--- build_scripts/deploy.sh
+++ build_scripts/deploy.sh
@@ -4,7 +4,8 @@ mkdir -p $HOME/.deploy/$KODI_VERSION
 cd $HOME/.deploy/$KODI_VERSION
 for f in $TRAVIS_BUILD_DIR/*.zip; do
   if [[ $f == *"$KODI_VERSION"* ]]; then
+    mkdir -p $TRAVIS_BUILD_DIR/.build/$KODI_VERSION/$(basename "$f" .zip)
     unzip $f -d $TRAVIS_BUILD_DIR/.build/$KODI_VERSION/$(basename "$f" .zip)
-    python3 $TRAVIS_BUILD_DIR/manage_repo.py $TRAVIS_BUILD_DIR -b $TRAVIS_BUILD_DIR/.build/$KODI_VERSION/$(basename "$f" .zip)/${app_id}
+    python3 $TRAVIS_BUILD_DIR/manage_repo.py $TRAVIS_BUILD_DIR -b $TRAVIS_BUILD_DIR/.build/$KODI_VERSION/$(basename "$f" .zip)/${APP_ID}
   fi
 done
