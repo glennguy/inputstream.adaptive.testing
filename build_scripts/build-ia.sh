97b28f28f365a984d06d3e29c4d5b1a54fa9f684
Glenn Guy <glennguy83@gmail.com>
2020-09-25 12:59:54 +1000

    Update build-ia.sh

--- build_scripts/build-ia.sh
+++ build_scripts/build-ia.sh
@@ -177,7 +177,7 @@ if [[ $KODI_VERSION == "leia" ]]; then
         touch $IA_HOME/patchapplied
     fi
 else
-    git checkout Matrix
+    git checkout master
     if [[ -f $IA_HOME/patchapplied ]]; then
         git apply -R Leia.patch
         rm $IA_HOME/patchapplied
