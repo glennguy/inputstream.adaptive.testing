1bf6f6b9d78f6b9329181ea4c75940d50b20f558
Glenn Guy <glennguy83@gmail.com>
2020-10-01 17:37:15 +1000

    Update build-ia.sh

--- build_scripts/build-ia.sh
+++ build_scripts/build-ia.sh
@@ -197,7 +197,7 @@ else
             ;;
     esac
     CMAKE_EXTRA_OPTIONS="-G $GENERATOR -T host=$HOST"
-    export PATH="$(cd '/c/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/VC/Auxiliary/Build' && cmd //c 'VcVarsAll.bat amd64 && C:\Program^ Files\Git\bin\bash.exe -c "printenv PATH"')"
+    export PATH="$(cd '/c/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/VC/Auxiliary/Build' && cmd /c "VcVarsAll.bat amd64 && bash -c 'printenv PATH'")"
     echo "exited?"
     cd '/c/Program Files/Git'
     ls -a
