3c2a76eefbf1266d9d2ecc0c6dbbdeb200437ce7
Glenn Guy <glennguy83@gmail.com>
2020-10-05 16:01:39 +1100

    deploy try 2

--- build_scripts/deploy.sh
+++ build_scripts/deploy.sh
@@ -30,7 +30,4 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git add .
 git commit --allow-empty -m "$TRAVIS_COMMIT_MESSAGE"
-git checkout Matrix
-git merge travis-build-$TRAVIS_BUILD_NUMBER
-git branch -d travis-build-$TRAVIS_BUILD_NUMBER
-git push
+git push --set-upstream origin travis-build-$TRAVIS_BUILD_NUMBER
